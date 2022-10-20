91a7c7b8ea1da4898bedf0693406cd9fc93e4266
Vanessa Bloom <ness.jade@outlook.com>
2022-10-20 18:52:58 +1000

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-	process.env.MONGODB_URI || 'mongodb://localhost:27017/list-it',
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-	}
+console.log(
+	'🚀 ~ file: connection.js ~ line 4 ~ process.env.MONGODB_URI',
+	process.env.MONGODB_URI
 );
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/list-it', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
