65e5e8c33b2f56a27c8fb49560440ec5ddfb002a
Vanessa Bloom <ness.jade@outlook.com>
2022-10-20 19:08:13 +1000

    deploying

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,8 +42,7 @@ const authLink = setContext((_, {headers}) => {
 const client = new ApolloClient({
 	// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
 	link: authLink.concat(httpLink),
-	cache: new InMemoryCache(),
-	persistedQueries: false,
+	cache: 'bounded',
 });
 
 function App() {
