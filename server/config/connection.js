afff892a7fb6da359612a6860df3414fb3119f08
Vanessa Bloom <ness.jade@outlook.com>
2022-10-20 18:25:11 +1000

    Pushing to Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+console.log(
+	'🚀 ~ file: connection.js ~ line 4 ~ process.env.MONGODB_URI',
+	process.env.MONGODB_URI
+);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/list-it', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
