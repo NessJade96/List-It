01372132def19384e7d43b12830fee95ce638d64
Vanessa Bloom <ness.jade@outlook.com>
2022-10-25 10:51:44 +1000

    live refresh fix

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
 }
 
-app.get('/', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'));
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../', 'client', 'dist', 'index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
